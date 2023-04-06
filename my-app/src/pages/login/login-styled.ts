542f108b8c1dca1c64d2ae58a5b8c580d56c36d1
Lucas Kahlil <kahlilpereira@gmail.com>
2023-04-06 19:13:39 -0300

    feat: create page login

---
+++ my-app/src/pages/login/login-styled.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const ContainerPage = styled.div `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+`
+
+export const ContainerForm = styled.div `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    width: 50%;
+    background-color: blue;
+`;
+
+export const ImgLogin = styled.img `
+    height: 100vh;
+    width: 50%;
+`;
