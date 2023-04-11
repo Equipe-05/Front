7617b0d2afa674270ce9a2703e8f4105899be28a
Lucas Kahlil <kahlilpereira@gmail.com>
2023-04-11 20:26:39 -0300

    feat: add api ands user request type

--- my-app/src/utils/api/api.ts
+++ my-app/src/utils/api/api.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosRequestConfig } from "axios";
-import { LoginRequest } from "../types/requests";
+import { CreateUser, UpdateUser, LoginRequest, } from "../types/requests";
 
 
 
@@ -22,14 +22,60 @@ axios.interceptors.request.use(
 );
 
 export const api = {
-    
+
     //LOGIN
 
     login: async ({ email, password }: LoginRequest) => {
         try {
-            const response = await axios.post("/auth/signed", { email, password })
+            const response = await axios.post("/auth/signin", { email, password })
             localStorage.setItem("token", response.data.token)
             return response.data
+        } catch (error) {
+            alert(error)
+        }
+    },
+
+    //USER 
+
+    postUser: async (data: CreateUser) => {
+        try {
+            const response = await axios.post("/user", data)
+            return response.data
+        } catch (error) {
+            alert(error)
+        }
+    },
+
+    getAllUsers: async () => {
+        try {
+            const response = await axios.get("/user")
+            return response.data
+        } catch (error) {
+            alert(error)
+        }
+    },
+
+    getUserById: async (data: UpdateUser) => {
+        try {
+            const response = await axios.get("/user/" + data.id)
+            return response.data
+        } catch (error) {
+            alert(error)
+        }
+    },
+
+    patchUserById: async (data: UpdateUser) => {
+        try {
+            const response = await axios.patch("/user/" + data.id, { name: data.name, email: data.email, role: data.role, password: data.password, confirmPassword: data.confirmPassword, cpf: data.cpf, address: data.address, phone: data.phone })
+            return response.data
+        } catch (error) {
+            alert(error)
+        }
+    },
+
+    deteleUserById: async (data: UpdateUser) => {
+        try {
+            const response = await axios.delete("/user/" + data.id)
         } catch (error) {
             alert(error)
         }
--- my-app/src/utils/types/requests.ts
+++ my-app/src/utils/types/requests.ts
@@ -9,3 +9,26 @@ export type LoginRequest = {
   email: string;
   password: string;
 }
+
+export type UpdateUser = {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+  password: string;
+  confirmPassword: string;
+  cpf: string;
+  address: string;
+  phone: string;
+}
+
+export type CreateUser = {
+  name: string;
+  email: string;
+  role: string;
+  password: string;
+  confirmPassword: string;
+  cpf: string;
+  address: string;
+  phone: string;
+}
