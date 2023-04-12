4e2e52c114298e8882069f21e56d50fec99f9011
vanderlei miguel <vanderleimiguel@hotmail.com>
2023-04-12 20:13:56 -0300

    fix: update login

--- my-app/src/utils/api/api.ts
+++ my-app/src/utils/api/api.ts
@@ -1,83 +1,100 @@
 import axios, { AxiosRequestConfig } from "axios";
-import { CreateUser, UpdateUser, LoginRequest, } from "../types/requests";
-
-
+import { CreateUser, UpdateUser, LoginRequest } from "../types/requests";
 
 axios.defaults.baseURL = "https://back-production-8e9e.up.railway.app";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 axios.interceptors.request.use(
-    function (config) {
-        const token = localStorage.getItem("token");
-        if (token) {
-            config.headers = {
-                Authorization: "Bearer " + token,
-            };
-        }
-        return config;
-    },
-    function (error) {
-        return Promise.reject(error);
+  function (config) {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers = {
+        Authorization: "Bearer " + token,
+      };
     }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
 );
 
 export const api = {
+  //LOGIN
 
-    //LOGIN
-
-    login: async ({ email, password }: LoginRequest) => {
-        try {
-            const response = await axios.post("/auth/signin", { email, password })
-            localStorage.setItem("token", response.data.token)
-            return response.data
-        } catch (error) {
-            alert(error)
-        }
-    },
-
-    //USER 
-
-    postUser: async (data: CreateUser) => {
-        try {
-            const response = await axios.post("/user", data)
-            return response.data
-        } catch (error) {
-            alert(error)
-        }
-    },
-
-    getAllUsers: async () => {
-        try {
-            const response = await axios.get("/user")
-            return response.data
-        } catch (error) {
-            alert(error)
-        }
-    },
-
-    getUserById: async (data: UpdateUser) => {
-        try {
-            const response = await axios.get("/user/" + data.id)
-            return response.data
-        } catch (error) {
-            alert(error)
-        }
-    },
-
-    patchUserById: async (data: UpdateUser) => {
-        try {
-            const response = await axios.patch("/user/" + data.id, { name: data.name, email: data.email, role: data.role, password: data.password, confirmPassword: data.confirmPassword, cpf: data.cpf, address: data.address, phone: data.phone })
-            return response.data
-        } catch (error) {
-            alert(error)
-        }
-    },
-
-    deteleUserById: async (data: UpdateUser) => {
-        try {
-            const response = await axios.delete("/user/" + data.id)
-        } catch (error) {
-            alert(error)
-        }
+  login: async ({ email, password }: LoginRequest) => {
+    try {
+      const response = await axios.post("/auth/signin", { email, password });
+      console.log();
+      localStorage.setItem("token", response.data.accessToken);
+      return response.data;
+    } catch (error) {
+      alert(error);
     }
-}
+  },
+
+  //GET
+  getUser: async () => {
+    try {
+      const response = await axios.get("/auth/signed");
+      return response.data;
+    } catch (error) {
+      alert(error);
+    }
+  },
+
+  //USER
+
+  postUser: async (data: CreateUser) => {
+    try {
+      const response = await axios.post("/user", data);
+      return response.data;
+    } catch (error) {
+      alert(error);
+    }
+  },
+
+  getAllUsers: async () => {
+    try {
+      const response = await axios.get("/user");
+      return response.data;
+    } catch (error) {
+      alert(error);
+    }
+  },
+
+  getUserById: async (data: UpdateUser) => {
+    try {
+      const response = await axios.get("/user/" + data.id);
+      return response.data;
+    } catch (error) {
+      alert(error);
+    }
+  },
+
+  patchUserById: async (data: UpdateUser) => {
+    try {
+      const response = await axios.patch("/user/" + data.id, {
+        name: data.name,
+        email: data.email,
+        role: data.role,
+        password: data.password,
+        confirmPassword: data.confirmPassword,
+        cpf: data.cpf,
+        address: data.address,
+        phone: data.phone,
+      });
+      return response.data;
+    } catch (error) {
+      alert(error);
+    }
+  },
+
+  deteleUserById: async (data: UpdateUser) => {
+    try {
+      const response = await axios.delete("/user/" + data.id);
+    } catch (error) {
+      alert(error);
+    }
+  },
+};
