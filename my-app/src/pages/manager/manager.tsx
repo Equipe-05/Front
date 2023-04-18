0ac0cf208ce8080339642a5cf71c32f435357dde
vanderlei miguel <vanderleimiguel@hotmail.com>
2023-04-17 22:59:01 -0300

    fix: refactor errors

--- my-app/src/pages/manager/manager.tsx
+++ my-app/src/pages/manager/manager.tsx
@@ -1,6 +1,4 @@
-import { CreateProduct } from "../createProduct/create-product";
 import MenuChoice from "../../components/menus/menuChoice";
-import UpdateProductForm from "../../components/forms/update/product/updateProduct";
 
 export function PageManager() {
   return (
