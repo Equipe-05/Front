b93731c6146cdcec735482cc17ba2165b6eaf55b
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-05 12:13:25 -0300

    feat(franchisee): refactor styled components

--- my-app/src/pages/franchisee/style.ts
+++ my-app/src/pages/franchisee/style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Manager = styled.div`
+export const Franchisee = styled.div`
   display: flex;
   min-height: 100vh;
   width: 100%;
@@ -12,4 +12,61 @@ export const Manager = styled.div`
 
 export const ImgStart = styled.img`
   max-height: 100vh;
+`;
+
+export const DashboardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-top: 10px;
+  align-items: center;
+  width: 1240px;
+  height: 1640px;
+  padding-bottom: 2rem;
+`;
+
+export const RowWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 10px 0;
+  width: 100%;
+`;
+
+export const SmallGraph = styled.div`
+  width: 100%;
+  height: 20%;
+  margin: 0 10px;
+`;
+
+export const LargeGraph = styled.div`
+  width: 50%;
+  height: 20%;
+  margin: 0 10px;
+`;
+
+export const TwoThirdsGraph = styled.div`
+  width: 67%;
+  height: 20%;
+  margin: 0 10px;
+`;
+
+export const OneThirdGraph = styled.div`
+  width: 32.5%;
+  height: 20%;
+  margin: 0 10px;
+  margin-right: 13px;
+`;
+
+export const FormGraph = styled(LargeGraph)`
+  height: 100%;
+  width: 48.5%;
+
+  &.first {
+    margin-right: 13px;
+    overflow: hidden;
+  }
+
+  &.second {
+    width: 48.9%;
+  }
 `;
