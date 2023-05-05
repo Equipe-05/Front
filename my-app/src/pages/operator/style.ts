1b5a124cb3aa333010b6e15b1e999911f9c9aeb9
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-05 12:13:01 -0300

    feat(operator): refactor styled components

--- my-app/src/pages/operator/style.ts
+++ my-app/src/pages/operator/style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Manager = styled.div`
+export const Operator = styled.div`
   display: flex;
   min-height: 100vh;
   width: 100%;
@@ -12,4 +12,44 @@ export const Manager = styled.div`
 
 export const ImgStart = styled.img`
   max-height: 100vh;
+`;
+
+export const DashboardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-top: 10px;
+  align-items: center;
+  width: 1240px;
+  height: 1650px;
+  padding-bottom: 3.5rem;
+`;
+
+export const RowWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 10px 0;
+  width: 100%;
+`;
+
+export const SmallGraph = styled.div`
+  width: 100%;
+  height: 20%;
+  margin: 0 10px;
+`;
+
+export const LargeGraph = styled.div`
+  width: 100%;
+  height: 20%;
+  margin: 0 10px;
+`;
+
+export const FullGraph = styled.div`
+  width: 100%;
+  margin: 0 10px;
+  height: 20%;
+`;
+
+export const FormGraph = styled(LargeGraph)`
+  height: 20%;
 `;
