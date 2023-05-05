72cd1ce063468f3dea9e3e5a09af6266ce804511
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-05 12:12:56 -0300

    feat(operator): refactor styled components

--- my-app/src/pages/operator/operator.tsx
+++ my-app/src/pages/operator/operator.tsx
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import DashboardCard09 from "../../components/dashboards/DashBoardCard09";
 import DashboardCard01 from "../../components/dashboards/DashboardCard01";
 import DashboardCard02 from "../../components/dashboards/DashboardCard02";
@@ -8,13 +7,21 @@ import DashboardCard07 from "../../components/dashboards/DashboardCard07";
 import DashboardCard10 from "../../components/dashboards/DashboardCard10";
 import DashboardCard11 from "../../components/dashboards/DashboardCard11";
 import MenuChoice from "../../components/menus/menuChoice";
-import { Manager } from "./style";
+import {
+  DashboardContainer,
+  FormGraph,
+  FullGraph,
+  LargeGraph,
+  Operator,
+  RowWrapper,
+  SmallGraph,
+} from "./style";
 
 export function PageOperator() {
   return (
     <div style={{ display: "flex", width: "100vw" }}>
       <MenuChoice />
-      <Manager>
+      <Operator>
         <DashboardContainer>
           <RowWrapper>
             <FormGraph>
@@ -52,60 +59,7 @@ export function PageOperator() {
             </FullGraph>
           </RowWrapper>
         </DashboardContainer>
-      </Manager>
+      </Operator>
     </div>
   );
 }
-
-const DashboardContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  margin-top: 10px;
-  align-items: center;
-  width: 1240px;
-  height: 1750px;
-  padding-bottom: 2rem;
-`;
-
-const RowWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin: 10px 0;
-  width: 100%;
-`;
-
-const SmallGraph = styled.div`
-  width: 100%;
-  height: 20%;
-  margin: 0 10px;
-`;
-
-const LargeGraph = styled.div`
-  width: 100%;
-  height: 20%;
-  margin: 0 10px;
-`;
-
-const TwoThirdsGraph = styled.div`
-  width: 66.66%;
-  height: 20%;
-  margin: 0 10px;
-`;
-
-const FullGraph = styled.div`
-  width: 100%;
-  margin: 0 10px;
-  height: 20%;
-`;
-
-const OneThirdGraph = styled.div`
-  width: 33%;
-  height: 20%;
-  margin: 0 10px;
-  margin-right: 13px;
-`;
-
-const FormGraph = styled(LargeGraph)`
-  height: 20%;
-`;
